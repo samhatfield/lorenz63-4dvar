9362e7943d45f5110976ecd372fd27d5997686a4
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2017-11-22 11:53:47 +0000

    Set max_iterations as the only constraint

--- main.f90
+++ main.f90
@@ -10,16 +10,15 @@ program lorenz63_4dvar
     real(dp), dimension(tstep,3) :: truth = 0.0_dp
     real(dp), dimension(tstep,3) :: best_guess
     real(dp) :: obs(tstep/freq,3)
-    real(dp) :: l(3), f, norm = 100.0_dp, initial(3)
+    real(dp) :: l(3), f, norm, initial(3)
     real(dp) :: time(tstep)
-    logical :: converged = .false.
     integer :: i, j = 1
 
     ! Check whether observation frequency divides into total number of timsteps
     if (mod(tstep, freq) /= 0) then
         stop 'Number of timesteps must be a multiple of the observation frequency'
     end if
-    
+
     ! Generate time axis
     time = (/ (real(i)*h, i = 0, tstep-1) /)
 
@@ -44,7 +43,7 @@ program lorenz63_4dvar
     initial = (/ 8.0_dp, 8.0_dp, 8.0_dp /)
 
     ! Perform minimisation
-    do while (.not. converged)
+    do j = 1, max_iterations
         ! Compute cost of current best guess
         best_guess = run_model(tstep, initial)
         f = calc_cost(tstep, best_guess, obs)
@@ -65,14 +64,6 @@ program lorenz63_4dvar
         l = l/norm
 
         initial = initial - 0.5_dp*l
-
-        ! Max number of iterations reached
-        if (j > max_iterations) then
-            converged = .true.
-            print *, 'Failed to converge in maximum number of iterations'
-        end if
-
-        j = j + 1
     end do
 
     call output(time, best_guess, "final_guess.txt")
