dd42e3bb24816857f23ddb5c97f0fe5338d9b008
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2017-11-23 11:58:33 +0000

    Fixed some bugs in the NL and TL models

--- lorenz63.f90
+++ lorenz63.f90
@@ -16,21 +16,19 @@ contains
         real(dp), intent(in) :: in(3)
         real(dp), dimension(tstep, 3) :: out
         real(dp), dimension(3) :: k1, k2
+        real(dp) :: x, y, z
         integer :: i
 
         out(1,:) = in
 
         do i = 1, tstep-1
-            k1 = h*(/&
-            & a*(out(i,2) - out(i,1)), &
-            & r*out(i,1) - out(i,2) - out(i,1)*out(i,3), &
-            & out(i,1)*out(i,2) - b*out(i,3) &
-            & /)
+            x = out(i,1); y = out(i,2); z = out(i,3)
+            k1 = h*(/ a*(y - x), r*x - y - x*z, x*y - b*z /)
 
             k2 = h*(/&
-            & a*(out(i,2) + k1(2) - out(i,1) - k1(1)), &
-            & r*(out(i,1) + k1(1)) - out(i,2) - out(i,2) - (out(i,1) + k1(1))*(out(i,3) + k1(3)), &
-            & (out(i,1) + k1(1))*(out(i,2) + k1(2)) - b*(out(i,3) + k1(3)) &
+            & a*(y + k1(2) - x - k1(1)), &
+            & r*(x + k1(1)) - y - k1(2) - (x + k1(1))*(z + k1(3)), &
+            & (x + k1(1))*(y + k1(2)) - b*(z + k1(3)) &
             & /)
 
             out(i+1,:) = out(i,:) + 0.5_dp * (k1 + k2)
@@ -39,26 +37,26 @@ contains
 
     function run_tangent_linear(tstep, in, in_p) result(out_p)
         integer, intent(in) :: tstep
-        real(dp), dimension(3), intent(in) :: in, in_p
-        real(dp), dimension(tstep, 3) :: out_p
+        real(dp), intent(in) :: in(tstep, 3), in_p(3)
+        real(dp) :: out_p(tstep,3)
         real(dp), dimension(3) :: k1, k1_p, k2_p
         real(dp) :: x, y, z, x_p, y_p, z_p
         integer :: i
 
         out_p(1,:) = in_p
 
-        do i = 1, tstep
-            x = in(i); x_p = out_p(i,1)
-            y = in(i); y_p = out_p(i,2)
-            z = in(i); z_p = out_p(i,3)
+        do i = 1, tstep-1
+            x = in(i,1); x_p = out_p(i,1)
+            y = in(i,2); y_p = out_p(i,2)
+            z = in(i,3); z_p = out_p(i,3)
 
             k1(1) = h*a*(y - x)
             k1(2) = h*(r*x - y - x*z)
             k1(3) = h*(x*y - b*z)
 
             k1_p(1) = h*a*(y_p - x_p)
-            k2_p(2) = h*(r*x_p - y_p - x_p*z - x*z_p)
-            k2_p(3) = h*(x_p*y + x*y_p - b*z_p)
+            k1_p(2) = h*(r*x_p - y_p - x_p*z - x*z_p)
+            k1_p(3) = h*(x_p*y + x*y_p - b*z_p)
 
             k2_p(1) = h*a*(y_p + k1_p(2) - x_p - k1_p(1))
             k2_p(2) = h*(r*(x_p + k1_p(1)) - y_p - k1_p(2) &
