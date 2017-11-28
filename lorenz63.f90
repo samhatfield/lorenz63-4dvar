0356f83909601ac0c608598f35835f129e0f70ea
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2017-11-28 17:20:11 +0000

    Added some comments to the Lorenz '63 module

--- lorenz63.f90
+++ lorenz63.f90
@@ -4,13 +4,15 @@ module lorenz63
     implicit none
 
     private
-    public run_model, run_tangent_linear, run_adjoint, a, b, r
+    public run_model, run_tangent_linear, run_adjoint
 
+    ! Model parameters
     real(dp), parameter :: a = 10.0_dp
     real(dp), parameter :: b = 8.0_dp/3.0_dp
     real(dp), parameter :: r = 28.0_dp
 
 contains
+    ! The ODE for the Lorenz '63 system
     function dRdT(state)
         real(dp), intent(in) :: state(3)
         real(dp) :: dRdT(3), x, y, z
@@ -20,6 +22,7 @@ contains
         dRdT = (/ a*(y - x), r*x - y - x*z, x*y - b*z /)
     end function dRdT
 
+    ! The Jacobian of the ODE for the Lorenz '63 system
     function jacob(state, dstate)
         real(dp), intent(in) :: state(3), dstate(3)
         real(dp) :: jacob(3), x, y, z, dx, dy, dz
@@ -30,6 +33,7 @@ contains
         jacob = (/ a*(dy - dx), r*dx - dy - dx*z - x*dz, dx*y + x*dy - b*dz /)
     end function jacob
 
+    ! The adjoint of the Jacobian of the ODE for the Lorenz '63 system
     function jacob_a(state, dstate_a)
         real(dp), intent(in) :: state(3), dstate_a(3)
         real(dp) :: jacob_a(3), x, y, z, dx_a, dy_a, dz_a
@@ -40,6 +44,7 @@ contains
         jacob_a = (/ -a*dx_a + (r-z)*dy_a + y*dz_a, a*dx_a - dy_a + x*dz_a, -x*dy_a - b*dz_a /)
     end function jacob_a
 
+    ! Run full nonlinear model using modified Euler scheme
     function run_model(tstep, in) result(out)
         integer, intent(in) :: tstep
         real(dp), intent(in) :: in(3)
@@ -47,6 +52,7 @@ contains
         real(dp), dimension(3) :: k1, k2
         integer :: i
 
+        ! First step
         out(1,:) = in
 
         do i = 1, tstep-1
@@ -57,6 +63,7 @@ contains
         end do
     end function run_model
 
+    ! Run tangent linear model
     function run_tangent_linear(tstep, in, din) result(dout)
         integer, intent(in) :: tstep
         real(dp), intent(in) :: in(tstep, 3), din(3)
@@ -64,6 +71,7 @@ contains
         real(dp), dimension(3) :: k1, dk1, dk2
         integer :: i
 
+        ! First step
         dout(1,:) = din
 
         do i = 1, tstep-1
@@ -75,6 +83,7 @@ contains
         end do
     end function run_tangent_linear
 
+    ! Run adjoint model
     function run_adjoint(tstep, in, din_a) result(dout_a)
         integer, intent(in) :: tstep
         real(dp), intent(in) :: in(:,:), din_a(3)
@@ -82,6 +91,7 @@ contains
         real(dp), dimension(3) :: k1, dk1_a, dk2_a
         integer :: i
 
+        ! First step
         dout_a = din_a
 
         do i = tstep-1, 1, -1
